2350a4f3a38ec7c9996007eb3e1bfa3a6e5d5021
gopal1704 <vgopalooty@gmail.com>
2021-03-29 13:42:15 +0530

    cc

--- src/components/CartItemList.jsx
+++ src/components/CartItemList.jsx
@@ -1,20 +1,48 @@
 import React from "react";
-import { List, ListItem, ListItemText ,ListItemSecondaryAction} from "@material-ui/core";
+import { DataGrid } from '@material-ui/data-grid';
 
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+} from "@material-ui/core";
+
+const columns = [
+  { field: "restaurantName", headerName: "restaurantName", width: 250 },
+  { field: "title", headerName: "Title", width: 500 },
+  { field: "qty", headerName: "Qty", width: 130 },
+  { field: "cost", headerName: "Cost", width: 130 },
+  {
+    field: "s_total",
+    headerName: "Amount",
+    width: 200,
+  },
+];
 export default function CartItemList(props) {
   return (
-    <List>
-      {props.cartitems.map((item) => {
-        return (
-          <ListItem button>
-            <ListItemText primary={item.title} />
-            <ListItemText primary={item.restaurantName} />
-            <ListItemText primary={item.qty} />
-            <ListItemText primary={item.cost} />
-            <ListItemSecondaryAction>   <ListItemText primary={item.s_total} /></ListItemSecondaryAction>
-          </ListItem>
-        );
-      })}
-    </List>
+    <div style={{ height: 400, width: "100%" }}>
+      <h1>Cart </h1>
+      <DataGrid
+        rows={props.cartitems}
+        columns={columns}
+        pageSize={100}
+        checkboxSelection
+      />
+    </div>
+
+    // <List>
+    //   {props.cartitems.map((item) => {
+    //     return (
+    //       <ListItem button>
+    //         <ListItemText primary={item.title} />
+    //         <ListItemText primary={item.restaurantName} />
+    //         <ListItemText primary={item.qty} />
+    //         <ListItemText primary={item.cost} />
+    //         <ListItemSecondaryAction>   <ListItemText primary={item.s_total} /></ListItemSecondaryAction>
+    //       </ListItem>
+    //     );
+    //   })}
+    // </List>
   );
 }
